f779c62f9886e778873e2ee2e0cf15ded3e8fb4e
Mohamed Fathy <moham6dfathy@gmail.com>
2024-09-25 22:56:22 +0300

    Deploy app

--- public/js/login.js
+++ public/js/login.js
@@ -26,10 +26,10 @@ export const logout = async () => {
   try {
     const res = await axios({
       method: 'GET',
-      url: 'http://127.0.0.1:3000/api/v1/users/logout',
+      url: '/api/v1/users/logout',
     });
 
-    if (res.data.status = 'success') location.reload(true);
+    if ((res.data.status = 'success')) location.reload(true);
   } catch (err) {
     showAlert('error', 'Loging Out fail ! Try again.');
   }
